c48eca8f73f269d608bdde76c7cc2b1de082af2b
Achappan <achuk233@gmail.com>
2023-12-11 12:34:27 +0530

    images added to the frontend module

---
+++ frontend-module/src/assets/images/index.ts
@@ -0,0 +1,2 @@
+export { default as loginPageFooter } from "./loginPageFooter.png";
+export { default as loginPageImage } from "./loginPageImage.png";
--- frontend-module/src/index.ts
+++ frontend-module/src/index.ts
@@ -1,2 +1,3 @@
 export { default as AuthUtils } from "./utils/authUtils";
 export { default as commonUtils } from "./utils/commonUtils";
+export { default as images } from "./assets/images";
