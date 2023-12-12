2a3060bc4213dcc6a7cffaac14468e09695568e1
Ashok Durai K <ashokduraik@gmail.com>
2023-12-12 20:48:04 +0530

    corrected achu changes

--- frontend-module/dist/assets/images/index.d.ts
+++
@@ -1,3 +0,0 @@
-import * as React from 'react';
-export declare const LoginPageFooter: () => React.JSX.Element;
-export declare const LoginPageImage: () => React.JSX.Element;
--- frontend-module/dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-export * from './assets/images';
-interface Props {
-    text: string;
-}
-export declare const ExampleComponent: ({ text }: Props) => React.JSX.Element;
-export { default as AuthUtils } from "./utils/authUtils";
-export { default as commonUtils } from "./utils/commonUtils";
